61498175a8b20b8057e0d76e89d7cb5e76638d34
kyrexar <kyrexar.c@gmail.com>
2020-03-04 16:27:43 +0100

    Wed 04/03

---
+++ AVD/Assets/adsosmar/lighting/scripts/Bullet3D.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet3D : MonoBehaviour
+{
+    public LayerMask layermask;
+    public float speed = 80f;
+    private Rigidbody RB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody>();
+        RB.AddForce(transform.forward * speed, ForceMode.Impulse);
+        Destroy(gameObject, 3f);
+    }
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+
+    private void OnTriggerEnter(Collider info)
+    {
+        Debug.Log(info.gameObject.name);
+        if //(info.gameObject.layer == Masks)
+            (layermask == (layermask | (1 << info.gameObject.layer)))
+        {
+            RB.Sleep();//avoid more interactions
+            GetComponent<Animator>().SetTrigger("Die");
+            //it plays the animation to make an explotion and die
+      
+        }
+
+    }
+}
---
+++ AVD/Assets/adsosmar/lighting/scripts/FireBullets3d.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBullets3d : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float frequency = 1f;
+    public GameObject Bullet3d;
+    public Transform[] BulletPositions;
+    public Animator[] GunsAnimators;
+    void Start()
+    {
+        StartCoroutine(Fire());
+    }
+    private int i=0;
+    IEnumerator   Fire()
+    {
+        //GunsAnimators[i].SetTrigger("Fire");
+
+        Instantiate(Bullet3d, BulletPositions[i].position, BulletPositions[i].rotation);
+        i++;
+        if (i >= BulletPositions.Length) i = 0;
+        yield return new WaitForSeconds(frequency);
+            
+        StartCoroutine(Fire());
+    }
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+}
