510dbfb74f8eb1ba889ad3a6372d64119c7a838a
kyrexar <kyrexar.c@gmail.com>
2020-03-06 19:02:26 +0100

    Fri6/3-2nd

--- AVD/Assets/adsosmar/lighting/scripts/Bullet3D.cs
+++ AVD/Assets/adsosmar/lighting/scripts/Bullet3D.cs
@@ -6,13 +6,14 @@ public class Bullet3D : MonoBehaviour
 {
     public LayerMask layermask;
     public float speed = 80f;
+    public float time = 3f;
     private Rigidbody RB;
     // Start is called before the first frame update
     void Start()
     {
         RB = GetComponent<Rigidbody>();
         RB.AddForce(transform.forward * speed, ForceMode.Impulse);
-        Destroy(gameObject, 3f);
+        Destroy(gameObject, time);
     }
     public void Die()
     {
